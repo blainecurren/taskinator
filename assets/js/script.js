eb8e91214f5645fed92302408010627386e35df0
Blaine Curren <blaine.curren@gmail.com>
2022-11-09 06:57:59 -0600

    added taskTypeInput var to get select dropdowns value property

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,14 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
   event.preventDefault();
 
+  // .value is the objects property
   var taskNameInput = document.querySelector("input[name='task-name']").value;
+  // get the value of the dropdowns selected element
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  listItemEl.textContent = taskNameInput;
   tasksToDoEl.appendChild(listItemEl);
 };
 
