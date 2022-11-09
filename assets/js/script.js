d387500635dcec261cba24151e2d4033abe6bf15
Blaine Curren <blaine.curren@gmail.com>
2022-11-09 06:52:59 -0600

    added var taskNameInput and selected .value property

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+  event.preventDefault();
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
